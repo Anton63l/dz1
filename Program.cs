0b26944c00947cd1f05df4e37fc7305edb95a330
Anton <anton@mail.ru>
2022-09-02 00:18:19 +0400

    Решена вторая задача

--- Program.cs
+++ Program.cs
@@ -55,3 +55,15 @@
 //          System.Console.WriteLine($"max = {c}");
 // }
 
+// >>>> Третья задача
+
+System.Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a%2==0){
+    System.Console.WriteLine($"Число {a} является четным");
+}
+else{
+        System.Console.WriteLine($"Число {a} является нечетным");
+
+}
